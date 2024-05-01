e3150999b70c4d693a1089e5ec6b32ddfabd465c
GersonJrr <mirandajunior.gm@gmail.com>
2024-05-01 13:08:22 -0300

    desafio finalizado

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using DesafioPOO.Models;
+
+
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia");
+            Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo1", imei: "11111111", memoria: 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+            Console.WriteLine($"Dados do seu Smartphone {nokia.Numero}, {nokia.Modelo}, {nokia.IMEI}, {nokia.Memoria}");
+
+
+            Console.WriteLine("Smartphone IPhone");
+            Smartphone iphone = new Iphone(numero: "4695", modelo: "Modelo2", imei: "222222222", memoria: 128);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Telegram");
+            Console.WriteLine($"Dados do seu Smartphone {iphone.Numero}, {iphone.Modelo}, {iphone.IMEI}, {iphone.Memoria}");
+        }
+    }
+}
